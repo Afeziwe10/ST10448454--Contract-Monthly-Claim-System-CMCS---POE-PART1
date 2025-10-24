872c2a94386906a91e665afb8a4523f797c86b09
Afeziwe Thandani <ST10448454@rcconnect.edu.za>
2025-10-24 23:45:24 +0200

    Helper folder for testing

---
+++ CMCS/CMCS/Helper/ClaimCalculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CMCS;
+namespace CMCS.Helper
+{
+    public class ClaimCalculator
+    {
+        public double? CalculateTotal(String hoursText, string rateText)
+        {
+            if (!double.TryParse(hoursText, out double hours) || !double.TryParse(rateText, out double rate))   
+                return null;
+
+            return hours * rate;
+        }
+    }
+}
