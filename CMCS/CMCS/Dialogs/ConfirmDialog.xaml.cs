cac2c26b8573353015ec8e61a5807bed9e60cb70
Afeziwe Thandani <ST10448454@rcconnect.edu.za>
2025-10-24 23:42:08 +0200

    Added dialogs for messages

---
+++ CMCS/CMCS/Dialogs/ConfirmDialog.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CMCS.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for ConfirmDialog.xaml
+    /// </summary>
+    public partial class ConfirmDialog : Window
+    {
+        public bool IsConfirmed { get; private set; } = false;
+        public ConfirmDialog(String message)
+        {
+            InitializeComponent();
+            MessageText.Text = message; 
+        }
+
+        private void Yes_Click(object sender, RoutedEventArgs e)
+        {
+            IsConfirmed = true;
+            this.Close();
+        }
+
+        private void No_Click (object sender, RoutedEventArgs e)
+        {
+            IsConfirmed=  false;
+            this.Close();
+        }
+    }
+}
---
+++ CMCS/CMCS/Dialogs/SuccessDialog.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+using System.Threading.Tasks;
+
+namespace CMCS.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for SuccessDialog.xaml
+    /// </summary>
+    public partial class SuccessDialog : Window
+    {
+        private bool _isClossing = false;
+        public SuccessDialog(string message)
+        {
+            InitializeComponent();
+            MessageText.Text = message;
+        }
+
+        private void BtnOk_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+       
+    }
+}
