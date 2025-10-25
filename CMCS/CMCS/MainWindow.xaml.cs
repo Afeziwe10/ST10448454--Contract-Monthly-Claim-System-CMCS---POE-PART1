2e18e3f9b13ec677fb24510945099086a9ef34a8
Afeziwe Thandani <ST10448454@rcconnect.edu.za>
2025-10-25 12:50:18 +0200

    Added an updated Main window

--- CMCS/CMCS/MainWindow.xaml.cs
+++ CMCS/CMCS/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿
 using CMCS.Pages;
+using CMCS.Users;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -25,20 +26,26 @@ namespace CMCS
 
         private void Lecture_Click(object sender, RoutedEventArgs e)
         {
-            new LecturerDashboard().Show();
-            //this.Close();
+            //new LecturerDashboard().Show();
+            LectureLoginPage loginPage = new LectureLoginPage();
+            loginPage.Show();
+            this.Close();
         }
 
         private void Coordinator_Click(object sender, RoutedEventArgs e)
         {
-            new CoordinatorDashboard().Show();
-            //this.Close();
+            //new CoordinatorDashboard().Show();
+            CordinatorLogin cordinatorLogin = new CordinatorLogin();
+            cordinatorLogin.Show();
+            this.Close();
         }
 
         private void Manager_Click(object sender, RoutedEventArgs e)
         {
-            new ManagerDashboard().Show();
-            //this.Close();
+            //new ManagerDashboard().Show();
+            ManagerLogin managerLogin = new ManagerLogin();
+            managerLogin.Show();
+            this.Close();
         }
     }
 }
